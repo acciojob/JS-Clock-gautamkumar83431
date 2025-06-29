9504f013b68fd28ee3d5c3a6a9d6a6c2ec2dc9fc
Gautam kumar <165561578+gautamkumar83431@users.noreply.github.com>
2025-06-29 21:42:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
